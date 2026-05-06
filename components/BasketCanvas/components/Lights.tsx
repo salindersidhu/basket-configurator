0ab1e3e4cb96219d9056ed85c66415ac4998782c
Salinder Sidhu <salinder.sid@gmail.com>
2026-05-05 23:39:14 -0400

    Improve 3D basket rendering, camera behavior, and scene structure

--- components/BasketCanvas/components/Lights.tsx
+++ components/BasketCanvas/components/Lights.tsx
@@ -6,7 +6,9 @@ export function Lights() {
         castShadow
         position={[150, 250, 200]}
         intensity={1.4}
-        shadow-mapSize={[1024, 1024]}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-normalBias={0.035}
       />
       <directionalLight position={[-100, 100, -100]} intensity={0.3} />
       <hemisphereLight args={[0xcccccc, 0x333333, 0.4]} />
